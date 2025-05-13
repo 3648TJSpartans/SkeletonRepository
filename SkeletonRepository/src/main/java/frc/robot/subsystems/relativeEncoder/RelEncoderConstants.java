c0f521a218201a03074a37b2e78e5013caaf3c32
Avery Vinlove <144643328+rakdosintolerant@users.noreply.github.com>
2025-05-13 13:46:14 -0600

    finished relEncoder comments

--- SkeletonRepository/src/main/java/frc/robot/subsystems/relativeEncoder/RelEncoderConstants.java
+++ SkeletonRepository/src/main/java/frc/robot/subsystems/relativeEncoder/RelEncoderConstants.java
@@ -1,24 +1,25 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.relativeEncoder;
 
-/** constants for the coral subsystem */
+import frc.robot.util.TunableNumber;
+
+/*
+ * Constants files contain numbers and other values
+ * that shouldn't change while robot code is running,
+ * except for tuning.
+ */
+
 public class RelEncoderConstants {
 
     public static final int relEncoderMotorCan = 9;
 
-    public static final double setpoint1 = 0;
-    public static final double setpoint2 = 1;
+    public static final double setpoint1 = new TunableNumber("relEncoder/setpoint1", 0).get();
+    public static final double setpoint2 = new TunableNumber("relEncoder/setpoint2", 1).get();
     public static final double relativeEncoderLimit = 2;
-    public static final double elevatorRange = 0.622;
-    public static final double heightToEncoderRatio = elevatorRange / relativeEncoderLimit;
 
-    public static final double kP = 0;
-    public static final double kI = 0;
-    public static final double kD = 0;
-    public static final double kFF = 0;
+    public static final double kP = new TunableNumber("relEncoder/kP", 0).get();
+    public static final double kI = new TunableNumber("relEncoder/kI", 0).get();
+    public static final double kD = new TunableNumber("relEncoder/kD", 0).get();
+    public static final double kFF = new TunableNumber("relEncoder/kFF", 0).get();
     public static final double odometryFrequency = 100;
     public static final double kMinRange = -1;
     public static final double kMaxRange = 1;
@@ -28,6 +29,6 @@ public class RelEncoderConstants {
 
     public static final int limitSwitchPin = 1;
 
-    public static final double marginOfError = 1;
+    public static final double marginOfError = new TunableNumber("relEncoder/marginOfError", 0).get();
 
 }
