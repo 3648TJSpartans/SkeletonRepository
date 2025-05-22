0d87de18d10376cbf743918394bf8be8bde61012
MicahCoder <micahgruenwald@gmail.com>
2025-05-21 18:54:40 -0600

    TX Fix

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "SkeletonRepository";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 19;
+  public static final String GIT_SHA = "e831583774c6df96a2a8c1824b3e28cb1c69e156";
+  public static final String GIT_DATE = "2025-05-21 16:06:53 MDT";
+  public static final String GIT_BRANCH = "gitIgnoreFix";
+  public static final String BUILD_DATE = "2025-05-21 18:50:31 MDT";
+  public static final long BUILD_UNIX_TIME = 1747875031660L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
