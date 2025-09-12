b719b6c5a9c13db5daca24183ab5b3a5dcb0facb
Micah Gruenwald <123602038+MicahCoder@users.noreply.github.com>
2025-09-12 10:54:40 -0600

    Commit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -43,6 +43,7 @@ import frc.robot.commands.absoluteEncoderCommands.AbsCmd;
 import frc.robot.commands.commandGroups.ExampleSequentialCmd;
 import frc.robot.commands.goToCommands.goToConstants.PoseConstants;
 import frc.robot.commands.goToCommands.DriveTo;
+import frc.robot.commands.goToCommands.DriveTo2;
 import frc.robot.commands.goToCommands.DriveToPose;
 import frc.robot.commands.goToCommands.goToConstants;
 import frc.robot.commands.goToCommands.goToConstants.PoseConstants.AutonState;
@@ -380,7 +381,7 @@ public class RobotContainer {
                                 m_drive).ignoringDisable(true));
                 Command driveTest = new DriveTo(m_drive, () -> PoseConstants.examplePose);
                 Pose2d alignOffset = new Pose2d(new Translation2d(.30, .30), new Rotation2d());
-                Command alignToTag = new DriveTo(m_drive, () -> m_vision.getTagRelativePose(),
+                Command alignToTag = new DriveTo2(m_drive, () -> m_vision.getTagRelativePose(),
                                 () -> alignOffset);
                 m_driveController.rightTrigger().whileTrue(driveTest);
                 m_driveController.leftTrigger().whileTrue(alignToTag);
---
+++ src/main/java/frc/robot/commands/goToCommands/DriveTo2.java
@@ -0,0 +1,100 @@
+package frc.robot.commands.goToCommands;
+
+import java.util.function.Supplier;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.drive.Drive;
+
+public class DriveTo2 extends Command {
+    private final Supplier<Pose2d> robotPoseSupplier;
+    private final Supplier<Pose2d> targetPoseSupplier;
+    private final Drive drive;
+    // Defines PID controlelrs
+    private final ProfiledPIDController driveController =
+            new ProfiledPIDController(goToConstants.drivekP, 0.0, goToConstants.drivekD,
+                    new TrapezoidProfile.Constraints(goToConstants.driveMaxVelocity,
+                            goToConstants.driveMaxAcceleration),
+                    0.02);
+
+    private final ProfiledPIDController thetaController =
+            new ProfiledPIDController(goToConstants.thetakP, 0.0, goToConstants.thetakD,
+                    new TrapezoidProfile.Constraints(goToConstants.thetaMaxVelocity,
+                            goToConstants.thetaMaxAcceleration),
+                    0.02);
+
+    public DriveTo2(Drive drive, Supplier<Pose2d> robotPose, Supplier<Pose2d> targetPose) {
+        this.robotPoseSupplier = robotPose;
+        this.targetPoseSupplier = targetPose;
+        this.drive = drive;
+        // Sets PID tolerances for at Goal.
+        driveController.setTolerance(goToConstants.driveTolerance);
+        thetaController.setTolerance(goToConstants.thetaTolerance);
+        // Means theta will take mimunimum path.
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
+        driveController.setGoal(0.0);
+        thetaController.setGoal(0.0);
+    }
+
+    public DriveTo2(Drive drive, Supplier<Pose2d> targetPose) {
+        this(drive, drive::getPose, targetPose);
+    }
+
+    @Override
+    public void execute() {
+        Pose2d robotPose = robotPoseSupplier.get();
+        Pose2d targetPose = targetPoseSupplier.get();
+        // Gets the displacement vector -- if the robot goes absolutely the wrong way,
+        // switch target pose and robot pose.
+        Translation2d displacement = targetPose.getTranslation().minus(robotPose.getTranslation());
+        // Defines translational speed the robot should go. displacement.getNorm is the
+        // magnitude of the displacement.
+        double driveSpeed = driveController.calculate(displacement.getNorm());
+        // sets velocity to the normalized displacement vector(direction of
+        // displacement) timesd the desired drive speed.
+        Translation2d setVelocity = displacement.div(displacement.getNorm()).times(driveSpeed);
+
+        // Gets the displacement angle -- if the robot rotates absolutely the wrong way,
+        // switch target pose and robot pose.
+        Rotation2d thetaDisplacement = targetPose.getRotation().minus(robotPose.getRotation());
+        // Gets PID for thera displacement
+        double thetaVelocity = thetaController.calculate(thetaDisplacement.getRadians());
+        // runs velocities
+        if (targetPose.equals(new Pose2d())) {
+            return;
+        }
+        drive.runVelocity(ChassisSpeeds.fromFieldRelativeSpeeds(setVelocity.getX(),
+                setVelocity.getY(), thetaVelocity, robotPose.getRotation()));
+
+        // Lets Log stuff
+        Logger.recordOutput("DriveTo2/displacement", displacement);
+        Logger.recordOutput("DriveTo2/RobotPose", robotPose);
+        Logger.recordOutput("DriveTo2/Trajectory", displacement);
+        Logger.recordOutput("DriveTo2/TargetPose", targetPose);
+        Logger.recordOutput("DriveTo2/setDriveSpeed", driveSpeed);
+        Logger.recordOutput("DriveTo2/setDriveVelocity", setVelocity);
+        Logger.recordOutput("DriveTo2/thetaDifference", thetaDisplacement);
+        Logger.recordOutput("DriveTo2/thetaVelocity", thetaVelocity);
+    }
+
+    public boolean atGoal() {
+        return driveController.atGoal() && thetaController.atGoal();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drive.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return atGoal();
+    }
+}
