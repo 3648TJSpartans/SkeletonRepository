8873aa5708f5db955d05125cd890bae501ab0cbd
Avery Vinlove <144643328+rakdosintolerant@users.noreply.github.com>
2025-11-13 14:27:47 -0700

    created diagnostic

---
+++ src/main/java/frc/robot/util/Diagnostics.java
@@ -0,0 +1,30 @@
+package frc.robot.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.function.BooleanSupplier;
+
+public class Diagnostics {
+
+    private static ArrayList<Requirement> requirements;
+
+    public static void putRequirements(String name, BooleanSupplier check) {
+        requirements.add(new Requirement(name, check));
+    }
+
+    public static boolean getRequirement(int index) {
+        return requirements.get(index).check.getAsBoolean();
+    }
+
+    public static class Requirement {
+
+        protected final String name;
+        protected final BooleanSupplier check;
+
+        public Requirement(String name, BooleanSupplier check) {
+            this.name = name;
+            this.check = check;
+        }
+    }
+
+}
