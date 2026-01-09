71b775ecff0e47a75cff38cac2c0f3c82b7c66d8
Micah Gruenwald <123602038+MicahCoder@users.noreply.github.com>
2026-01-09 14:07:09 -0700

    Fix conflicting CAN

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -142,7 +142,6 @@ public class RobotContainer {
                 // CAN 10
                 m_exampleFlywheel = new RelEncoderSparkMax(
                                 new MotorConfig("Flywheel").motorCan(10).Ks(0.0).Kv(0.0));
-                                new MotorConfig("Flywheel").motorCan(2).Ks(0.0).Kv(0.0));
                 Logger.recordOutput("Poses/shouldFlip", AllianceFlipUtil.shouldFlip());
                 Logger.recordOutput("Override", override);
                 override = false;
